8d288fbfe28a4919772c53ea5dc03d5bac8ddf93
Prasad00015 <bhavani00015@gmail.com>
2022-11-04 16:22:49 +0530

    file sending

---
+++ employee.java
@@ -0,0 +1,14 @@
+class Employee
+{
+ public static void main(String args[])
+ {
+  int id=78;
+  String name="geetha";
+  String address="rajahmundry";
+  float salary=50000;
+  System.out.println("Id:"+id);
+  System.out.println("Name:"+name);
+  System.out.println("Address:"+address);
+  System.out.println("Salary:"+salary);
+ }
+}
